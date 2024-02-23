07886cb66230dc65dfc856e30382f39da5746871
Connor1719 <geoman.cr@gmail.com>
2024-02-23 13:37:43 -0700

    ast

--- script.py
+++ script.py
@@ -36,6 +36,7 @@ def run_tests():
 
 def exec_prog_comp(in_file, out_file, test):
     cla =  [f"{config['paths']['path_to_makefile_and_exec']}/compile", "/compile"]
+    b = True if test == 'test_ast' else False
     if (config['params'][f'{test}_param']):
         cla.append(config['params'][f'{test}_param'])
     with open(in_file, 'rb') as inpt:
@@ -45,11 +46,17 @@ def exec_prog_comp(in_file, out_file, test):
             result = subprocess.run(cla, stdin=inpt, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         except subprocess.CalledProcessError as e:
             if e.returncode == 1:
+                if b:
+                    compare_cases(in_file, otpt, "1", test, e.stdout.decode(), b)
+                    return
                 compare_cases(in_file, otpt, "1", test, e.stderr.decode())
             else:
                 print(f"Unknown error on {test} -> {e}\n")
         else:
             if result.returncode == 0:
+                if b:
+                    compare_cases(in_file, otpt, "0", test, e.stdout.decode(), b)
+                    return
                 compare_cases(in_file, otpt, "0", test)
             else:
                 print(f"Unknown return code on {test}")
@@ -60,11 +67,20 @@ def check_output(output_file):
     return output_file[0] == "1" and len(output_file) > 1 and not first_three(output_file[1])
 
 
-def compare_cases(in_file, out_file, output_code, test, out = None):
+def compare_cases(in_file, out_file, output_code, test, out = None, b: bool = False):
     in_file = in_file[in_file.rfind("/")+1:]
     if out_file[0] == "0" and output_code == "0":
-        print(f"{config['print_map'][test]} PASSED {in_file}\n")
-        inc_pass()
+        if not b:
+            print(f"{config['print_map'][test]} PASSED {in_file}\n")
+            inc_pass()
+        elif b:
+            if out == out_file[1]:
+                print(f"{config['print_map'][test]} PASSED {in_file}\n")
+                inc_pass()
+            else:
+                print(f"{config['print_map'][test]} FAILED {in_file}")
+                print(f"    Expected: Exit status 0 -> {out_file[1]}")
+                print(f"      Actual: {out}\n")
     elif out_file[0] == "0" and output_code == "1":
         print(f"{config['print_map'][test]} FAILED {in_file}")
         print("    Expected: Exit status 0")
@@ -74,17 +90,33 @@ def compare_cases(in_file, out_file, output_code, test, out = None):
         print(f"    Expected: Exit status 1 -> {first_three(out_file[1])}")
         print("      Actual: Exit status 0\n")
     elif out_file[0] == "1" and output_code == "1":
-        try:
-            if first_three(out) == first_three(out_file[1]):
-                print(f"{config['print_map'][test]} PASSED {in_file}\n")
-                inc_pass()
-            else:
-                print(f"{config['print_map'][test]} FAILED {in_file}")
-                print(f"    Expected: Exit status 1 -> {first_three(out_file[1])}")
-                print(f"      Actual: Exit status 1-> {out}\n")
-        except:
-            print(f"{config['print_map'][test]} FAILED {in_file}\n")
-            print("ERROR OCCURED DURING COMPARISON\n")
+        # PARSING/SEMANTIC
+        if not b:
+            try:
+                if first_three(out) == first_three(out_file[1]):
+                    print(f"{config['print_map'][test]} PASSED {in_file}\n")
+                    inc_pass()
+                else:
+                    print(f"{config['print_map'][test]} FAILED {in_file}")
+                    print(f"    Expected: Exit status 1 -> {first_three(out_file[1])}")
+                    print(f"      Actual: Exit status 1-> {out}\n")
+            except:
+                print(f"{config['print_map'][test]} FAILED {in_file}\n")
+                print("ERROR OCCURED DURING COMPARISON\n")
+        # AST fail
+        elif b:
+            try:
+                if out == out_file[1]:
+                    print(f"{config['print_map'][test]} PASSED {in_file}\n")
+                    inc_pass()
+                else:
+                    print(f"{config['print_map'][test]} FAILED {in_file}")
+                    print(f"    Expected: Exit status 1 -> {first_three(out_file[1])}")
+                    print(f"      Actual: Exit status 1-> {out}\n")
+            except:
+                print(f"{config['print_map'][test]} FAILED {in_file}\n")
+                print("ERROR OCCURED DURING COMPARISON\n")
+
     else:
         print(f"ERROR WITH CODES PARSING {test} {output_code} {out_file[0]}")
     inc_total()
